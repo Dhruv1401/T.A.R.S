0f82fad55b6264f7441b7597f5b22885dbf68d29
Dhruv1401 <dhruv.y.jadav@gmail.com>
2025-04-05 15:16:07 +0530

    Modified main.py to work with terminal and terminal_vb

--- tars/main.py
+++ tars/main.py
@@ -1,4 +1,5 @@
-from tars.interfaces.terminal_vb import run_terminal_ui
+from tars.interfaces.terminal_ui import run_terminal_ui
 
 if __name__ == "__main__":
     run_terminal_ui()
+#Upgraded main.py to work with terminal and terminal_vb
